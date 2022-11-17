6c2304a6a06788b283c4f020f97d3cfd8aebd296
flakennn <qwer2323vex@yandex.ru>
2022-11-17 21:29:49 +0300

    123

--- src/Nomer4/Main.java
+++ src/Nomer4/Main.java
@@ -1,14 +1,14 @@
-package Nomer3;
+package Nomer4;
 
 import java.util.regex.*;
 public class Main {
     public static void main(String[] args) {
-        String regex = "(d{2})[.](d{2})[.](d{4})*";
-        String s = "12.32.1234";
+        String regex = "(\\w+)@(\\w+\\.)(\\w+)(\\.\\w+)*" ;
+        String s = "ananas@mail.ru shtaket@com MrCredo@yandex.ru united@grib@.ru";
         Pattern p1 = Pattern. compile  (regex);
         Matcher m1 = p1.matcher(s);
         while (m1.find()) {
-            System.out.println("Валюта: " + m1.group());
+            System.out.println("email: " + m1.group());
         }
     }
 }
